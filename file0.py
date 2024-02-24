35b303a6ad36375e687750ac6003fa356a58b9e6
Omama K <omamak1998@gmail.com>
2024-02-24 18:29:25 +0300

    hello world label widget

---
+++ file0.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+root = Tk()
+# creating Label widget
+myLabel = Label(root, text="Hello, world!")
+
+# displaying the widget on screen
+myLabel.pack()
+
+# event loop
+root.mainloop()
